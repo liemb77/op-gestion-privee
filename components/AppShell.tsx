0295f530732cd49dce5f39c92727df0c18739df6
liemb77 <liem@nestlineautomation.ca>
2026-06-08 17:12:04 -0400

    Force page remount on language switch via key prop

--- components/AppShell.tsx
+++ components/AppShell.tsx
@@ -1,14 +1,23 @@
 'use client';
-import { LanguageProvider } from '@/contexts/LanguageContext';
+import { LanguageProvider, useLang } from '@/contexts/LanguageContext';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import { ReactNode } from 'react';
+
+function PageContent({ children }: { children: ReactNode }) {
+  const { lang } = useLang();
+  return (
+    <main className="flex-1" key={lang}>
+      {children}
+    </main>
+  );
+}
 
 export default function AppShell({ children }: { children: ReactNode }) {
   return (
     <LanguageProvider>
       <Navbar />
-      <main className="flex-1">{children}</main>
+      <PageContent>{children}</PageContent>
       <Footer />
     </LanguageProvider>
   );
